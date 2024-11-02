347f9c77709ea11b4a7bdd5d92ac094417d300ae
Jack <62919016+JackDunfey@users.noreply.github.com>
2024-11-02 03:37:18 -0400

    bit of a fixer upper

--- arp/arp_kernel/arpk.c
+++ arp/arp_kernel/arpk.c
@@ -14,7 +14,7 @@ static unsigned int arp_filter_hook(void *priv,
 
     if (ntohs(eth->h_proto) == ETH_P_ARP) {
         arp = (struct arphdr *)(skb->data + sizeof(struct ethhdr));
-        if (arp->op == htons(ARPOP_REPLY)) {
+        if (arp->ar_op == htons(ARPOP_REPLY)) {
             printk(KERN_INFO "Filtered outgoing ARP reply\n");
             return NF_DROP; // Drop the ARP reply
         }
