a207ee1d44f97811c5414a209c8a32b9558571ff
Jack <62919016+JackDunfey@users.noreply.github.com>
2024-11-18 16:18:49 -0500

    savestate

---
+++ arp/pf/setup.sh
@@ -0,0 +1,5 @@
+fetch https://www.python.org/ftp/python/3.13.0/Python-3.13.0.tar.xz
+tar -xvf Python-3.13.0.tar.xz
+cd /usr
+fetch https://download.freebsd.org/ftp/ports/ports/ports.tar.xz
+tar -xvf ports.tar.gz
